a8f7ce20a5bfe01149c21dd0d747c120b79aaedd
Tulande <johancagarcia17@gmail.com>
2025-07-01 21:10:44 -0500

    Add files via upload

---
+++ Maps/Map_2017/styles/LosNevados_2017_1_style.js
@@ -0,0 +1,121 @@
+var size = 0;
+var placement = 'point';
+function categories_LosNevados_2017_1(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case '3':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(31,141,73,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '11':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(81,151,153,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '13':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(216,159,92,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '21':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(255,239,195,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '23':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(255,160,122,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '24':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(212,39,30,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '25':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(219,77,79,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '27':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(255,255,255,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '29':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(255,170,95,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '33':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(33,45,203,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '34':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(144,219,225,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_LosNevados_2017_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("DN");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_LosNevados_2017_1(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
